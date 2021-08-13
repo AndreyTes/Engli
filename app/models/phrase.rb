b9a439c420f90849c861e711d726ef66705680e0
AndreyTes <andrey_tes2@mail.ru>
2021-08-13 13:30:59 +0300

    Fix phrase model

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -16,7 +16,7 @@ accepts_nested_attributes_for :examples, allow_destroy: true
 
 validates :phrase, presence: true, uniqueness: true
 validates :translation, :user_id, presence: true
-validates :category, inclusion: { in: %w(Actions Time Productivity Apologies),
+validates :category, inclusion: { in: %w(Actions Time Productivity Apologies Common),
   message: "%{value} is not a valid categoty" }
 enum category: {Actions: 0, Time: 1, Productivity: 2, Apologies: 3, Common: 4}
 end
